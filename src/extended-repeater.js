1944a16b01e928c5b96911507707713dd393192c
tomsonst <40933547+tomsonst@users.noreply.github.com>
2020-10-03 22:34:40 +0300

    feat: add solutions chipher and extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = '';
+  if(options.repeatTimes === undefined){
+    options.repeatTimes = 1;
+  }
+  for(let i = 0; i < options.repeatTimes; i++){
+    result += str;
+    if (options.additionRepeatTimes === undefined && typeof(options.addition) === 'string'){
+      options.additionRepeatTimes = 1;
+    }
+    for (let i = 0; i < options.additionRepeatTimes; i++){
+      result += options.addition;
+      if(i !== options.additionRepeatTimes -1){
+        result += options.additionSeparator;
+      }
+    }
+    if(i !== options.repeatTimes - 1){
+      if (options.separator){
+        result += options.separator;
+      } else {
+        result += '+';
+      }
+    }
+  }
+  return(result);
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,74 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(m, k) {
+    try {
+    let message = m.toLowerCase();
+    let key = k.toLowerCase();
+		if (key.length < message.length) {
+      let keyWord = '';
+      message = message.toLowerCase();
+			while (keyWord.length < message.length) {
+				keyWord += key;
+			}
+			key = keyWord.slice(0, message.length - 1);
+		}
+		let result = '';
+		let j = 0;
+		for (let i = 0; i < message.length; i++) {
+			if (message[i] === ' ') {
+				result += ' ';
+			} else if (/^[a-zA-Z]{0,}$/.test(message[i]) === false) {
+				result += message[i];
+			} else {
+				let symbolCharCode =
+					(message[i].charCodeAt() - 96 + key[j].charCodeAt() - 96) % 26;
+        	if (symbolCharCode < 1) {
+					symbolCharCode = 26 + symbolCharCode;
+				}
+				let b = String.fromCharCode(symbolCharCode + 95);
+				result += b;
+				j++;
+			}
+		}
+    return(result.toUpperCase());
+    } catch(e){
+      throw new Error('THROWN');
+    }
+	}
+	decrypt(m, k) {
+    try {
+    let message = m.toLowerCase();
+    let key = k.toLowerCase();
+		let keyWord = '';
+		message = message.toLowerCase();
+		while (keyWord.length < message.length) {
+			keyWord += key;
+		}
+		key = keyWord.slice(0, message.length - 1);
+		let result = '';
+		let j = 0;
+		for (let i = 0; i < message.length; i++) {
+			if (message[i] === ' ') {
+				result += ' ';
+			} else if (/^[a-zA-Z]{0,}$/.test(message[i]) === false) {
+				result += message[i];
+			} else {
+				let symbolCharCode =
+					((message[i].charCodeAt() - 96) - (key[j].charCodeAt() - 96)) % 26;
+				if (symbolCharCode < 0) {
+					symbolCharCode = 26 + symbolCharCode;
+				}
+				let b = String.fromCharCode(symbolCharCode + 97);
+				result += b;
+				j++;
+			}
+		}
+    return(result.toUpperCase());
+  } catch(e){
+    throw new Error('THROWN');
   }
+	}
 }
 
 module.exports = VigenereCipheringMachine;
